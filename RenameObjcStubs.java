e17d7b0009917fc8f3d60998ab402ce8b815da4e
level3tjg <t.grantham14@gmail.com>
2024-04-02 06:54:44 -0400

    use MemoryBlock instead of parsing MachHeader again

--- RenameObjcStubs.java
+++ RenameObjcStubs.java
@@ -3,8 +3,6 @@
 //@category iOS
 
 import ghidra.app.script.GhidraScript;
-import ghidra.app.util.bin.*;
-import ghidra.app.util.bin.format.macho.*;
 import ghidra.program.model.mem.*;
 import ghidra.program.model.symbol.*;
 import ghidra.program.model.listing.*;
@@ -19,20 +17,14 @@ public class RenameObjcStubs extends GhidraScript {
     	if (!format.contains("Mach-O"))
     		throw new Exception("Executable is not Mach-O");
     	
-    	for (MemoryBlock block : getMemoryBlocks())
-    		if (block.getName().contains("__bss") && !block.isInitialized())
-    			currentProgram.getMemory().convertToInitialized(block, (byte)0);
-    	
-    	MemoryByteProvider provider = new MemoryByteProvider(currentProgram.getMemory(), currentProgram.getMinAddress());
-    	MachHeader header = new MachHeader(provider).parse();
-    	Section stubsSection = header.getSection("__TEXT", "__objc_stubs");
+    	MemoryBlock stubsBlock = getMemoryBlock("__objc_stubs");
     	
     	for (Function func : functionManager.getFunctions(false)) {
     		Address entryPoint = func.getEntryPoint();
     		long offset = entryPoint.getOffset();
     		
-    		if (offset < stubsSection.getAddress()) break;
-    		if (offset > stubsSection.getAddress() + stubsSection.getSize()) continue;
+    		if (offset < stubsBlock.getStart().getOffset()) break;
+    		if (offset > stubsBlock.getStart().getOffset() + stubsBlock.getSize()) continue;
     		
     		CodeUnit unit = currentProgram.getListing().getCodeUnitAfter(entryPoint);
     		Reference[] references = unit.getOperandReferences(1);
